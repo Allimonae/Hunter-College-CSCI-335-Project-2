88cd6d25d12aa809c63b8a2f5bd24fd2ba069aea
Allimonae <128328789+Allimonae@users.noreply.github.com>
2024-04-10 20:58:34 -0400

    comments

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include <chrono>
 
 int main(){
+    // Reads from a file and populates a dynamic vector based on the instructions provided in the file
     std::vector<int> *instructions = new std::vector<int>;
 
     std::ifstream file("testinput.txt");
@@ -21,6 +22,8 @@ int main(){
     }
     std::string line;
     std::string instr, token;
+
+    // Each "insert" operation adds the provided value to the vector, and each "pop" operation adds a (-1) to the vector.
     while(file >> instr >> token){
         if (instr == "insert"){
             instructions->push_back(stoi(token));
@@ -29,32 +32,14 @@ int main(){
             instructions->push_back(-1);
         }
     }
-    
-    // std::vector<int> vec = {14667, 3074, 11294, 26952, 11167, 8956, 9292, 15789, 10697, 3350, 30176};
-    // AvlTree small;
-    // AvlTree large;
-
-    // for (auto it = vec.begin(); it != vec.end(); ++it){
-    //     treeInsert(small, large, *it);
-    // }
-    
-    // std::cout << "small: "; 
-    // small.print(); 
-    // std::cout << std::endl;
-    // std::cout << "large: "; 
-    // large.print(); 
-    // std::cout << std::endl;
-    // std::cout << treePopMedian(small, large);
 
     auto beg = std::chrono::high_resolution_clock::now();
  
     treeMedian(instructions);
- 
     
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - beg);
     std::cout << "\nElapsed Time: " << duration.count() << " microseconds";
-    
 
     delete instructions;
 }
